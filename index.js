c0dbae991e237efe0bbbd6b14b8cdccf246c4913
MarcoBrunoBR <marco.bruno.br@gmail.com>
2018-06-18 20:52:43 -0300

    Primeiro exercício

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const port = 8080
+const ip = 'localhost' // 127.0.0.1 || 0.0.0.0
+
+const server = http.createServer((req, res) => {
+  const rota = []
+
+  rota['/'] = '<h1>Home</h1>'
+  rota['/contato'] = '<h1>Contato</h1>'
+
+  res.end(rota[req.url] || '<meta charset="utf-8"><h1>Não tem!</h1>')
+})
+
+server.listen(port, ip, () => {
+  console.log('Servidor de pé em http://' + ip + ':' + port)
+  console.log(`Servidor de pé em http://${ip}:${port}`)
+  console.log('Pra derrubar o servidor: ctrl + c')
+})
